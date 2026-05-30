58891d722e961a7984173d8b9963401d873c586f
Advaith_Hello <advaith.anoopr@gmail.com>
2026-05-30 13:42:37 -0500

    Monitor seperation

---
+++ hyprscripts/move_to_workspace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+import json
+import subprocess
+
+res = subprocess.run(["hyprctl", "activeworkspace", "-j"], capture_output=True)
+monitor_id = json.loads(res.stdout)["monitorID"]
+target_workspace = int(sys.argv[1]) + (monitor_id * 10)
+subprocess.run(["hyprctl", "dispatch", "movetoworkspace", str(target_workspace)])
